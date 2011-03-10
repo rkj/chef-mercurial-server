94ff33faf73c41e6f696135ff0c192edd4145abe
Roman Kamyk <roman.kamyk@gmail.com>
2011-03-10 12:31:51 +0100

    paths

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
 default[:mercurial][:server][:admin] = "root"
+default[:mercurial][:server][:repos] = "/var/lib/mercurial-server/repos/"
+default[:mercurial][:server][:etc] = "/etc/mercurial-server/"
 
